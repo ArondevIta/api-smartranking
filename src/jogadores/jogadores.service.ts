2a383ad291c37a55b7a4665553d9c8ba342bebed
ArondevIta <aronamaral@gmail.com>
2021-05-29 14:30:40 -0300

    first commit

---
+++ src/jogadores/jogadores.service.ts
@@ -0,0 +1,81 @@
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { CriarJogadorDto } from './dtos/criar-jogador.dto';
+import { Jogador } from './interfaces/jogador.interface';
+import { v4 as uuidv4 } from 'uuid';
+
+@Injectable()
+export class JogadoresService {
+  private jogadores: Jogador[] = [];
+
+  private readonly logger = new Logger(JogadoresService.name);
+
+  async criarAtualizarJogador(criarJogadorDto: CriarJogadorDto): Promise<void> {
+    const { email } = criarJogadorDto;
+
+    const jogadorEncontrado = this.jogadores.find(
+      (jogador) => jogador.email === email,
+    );
+
+    if (jogadorEncontrado) {
+      return this.atualizar(jogadorEncontrado, criarJogadorDto);
+    } else {
+      this.criar(criarJogadorDto);
+    }
+  }
+
+  async consultarTodosJogadores(): Promise<Jogador[]> {
+    return this.jogadores;
+  }
+
+  async consultarJogadorPeloEmail(email: string): Promise<Jogador> {
+    const jogadorEncontrado = this.jogadores.find(
+      (jogador) => jogador.email === email,
+    );
+
+    if (!jogadorEncontrado) {
+      throw new NotFoundException(`Jogador não encontrado`);
+    }
+
+    return jogadorEncontrado;
+  }
+
+  async deletarJogador(email: string): Promise<void> {
+    const jogadorEncontrado = this.jogadores.find(
+      (jogador) => jogador.email === email,
+    );
+
+    if (!jogadorEncontrado) {
+      throw new NotFoundException(`Jogador não encontrado`);
+    }
+
+    this.jogadores = this.jogadores.filter(
+      (jogador) => jogador.email !== email,
+    );
+  }
+
+  private criar(criarJogadorDto: CriarJogadorDto): void {
+    const { email, nome, telefoneCelular } = criarJogadorDto;
+
+    const jogador: Jogador = {
+      _id: uuidv4(),
+      nome,
+      telefoneCelular,
+      email,
+      ranking: 'A',
+      posicaoRanking: 1,
+      urlFotoJogador:
+        'https://www.google.com.br/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png',
+    };
+
+    this.logger.log(`criaJogadorDto: ${JSON.stringify(jogador)}`);
+    this.jogadores.push(jogador);
+  }
+
+  private atualizar(
+    jogadorEncontrado: Jogador,
+    criarJogadorDto: CriarJogadorDto,
+  ): void {
+    const { nome } = criarJogadorDto;
+    jogadorEncontrado.nome = nome;
+  }
+}
