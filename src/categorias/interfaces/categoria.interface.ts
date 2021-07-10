37bb4e4f2f655fe828b7ca81b6bd0269d5f1bfe3
ArondevIta <aronamaral@gmail.com>
2021-07-10 12:20:08 -0300

    categorias parte 1

---
+++ src/categorias/interfaces/categoria.interface.ts
@@ -0,0 +1,15 @@
+import { Document } from 'mongoose';
+import { Jogador } from 'src/jogadores/interfaces/jogador.interface';
+
+export interface Evento {
+  nome: string;
+  operação: string;
+  valor: number;
+}
+
+export interface Categoria extends Document {
+  readonly categoria: string;
+  descticao: string;
+  eventos: Array<Evento>;
+  jogador: Array<Jogador>;
+}
